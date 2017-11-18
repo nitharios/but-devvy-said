8336d95fff8c08c1eab98918755b8b2c96aea17d
Nathan N <Nitharios@users.noreply.github.com>
2017-11-17 17:04:22 -1000

    Initial server sanity check

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('Server request');
+  res.json('Sanity check');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
