1c4ad7962971e35f163481880335f9b424d0abea
Nathan N <Nitharios@users.noreply.github.com>
2017-11-18 15:35:14 -1000

    Cleaned up code

--- server/server.js
+++ server/server.js
@@ -3,19 +3,19 @@ const bodyParser = require('body-parser');
 // const db = require('./models');
 const DevvyCho = require('./chatbot');
 const dotenv = require('dotenv');
-
-const PORT = process.env.PORT || 8000;
-
-const app = express();
-
 // load env variables
 dotenv.load();
+
+const PORT = process.env.PORT || 8000;
+const SLACK_TOKEN = process.env.SLACK_TOKEN || '';
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
 // connects Devvy to slack
-DevvyCho(process.env.SLACK_TOKEN);
+DevvyCho(SLACK_TOKEN);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
