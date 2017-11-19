05db60594996c24dd422becc24d9afcc70735efa
Nathan N <Nitharios@users.noreply.github.com>
2017-11-19 12:29:02 -1000

    Added indexing to models

--- server/models/Resource.js
+++ server/models/Resource.js
@@ -1,20 +1,37 @@
 /*This can be a link to a video, tutorial, or StackOverflow Q&A. It can also be a code or text snippet from a DevLeague student.*/
 
 module.exports = function (sequelize, DataTypes) {
-
   const Resource = sequelize.define('Resource', {
-    url : {type : DataTypes.STRING, allowNull : true}
+    url : {
+      type : DataTypes.STRING, 
+      allowNull : true
+    }
   },
-    {tableName : 'resources', timestamps : false}
-  );
+  {
+    tableName : 'resources'
+  },
+  {
+    indexes : [
+      {
+        name : 'resources_pk',
+        unique : true,
+        fields : ['id']
+      }
+    ]
+  });
 
-  Resource.associate = function (models) {
+
+  Resource.associate = function(models) {
     Resource.belongsTo(models.Topic, {
-      foreignKey : 'topic_id',
-      as: 'Topic'
+      foreignKey : 'topic_id'
     });
-    Resource.belongsToMany(models.Tag, {through : 'tags', foreignKey : 'tag_id'});
-  }
+    
+    Resource.belongsToMany(models.Tag, {
+      through : 'tags', 
+      foreignKey : 'tag_id',
+      onDelete : 'CASCADE'
+    });
+  };
 
   return Resource;
-}
+};
