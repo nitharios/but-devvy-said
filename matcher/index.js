6735d4c7fac7119a2b375223876e15ffc276ca6f
Justin Lee <leejwg.3@gmail.com>
2017-11-18 14:04:19 -1000

    vanila style from the terminal.

---
+++ matcher/index.js
@@ -0,0 +1,26 @@
+// jshint esversion:6
+const patterns = require('../patterns');
+const XRegExp = require('xregexp');
+
+let createEntities = (str, pattern) => {
+  return XRegExp.exec(str, XRegExp(pattern, "i"));
+};
+
+let matchPattern = (str, cb) => {
+  let getResult = patterns.find(item => {
+    if(XRegExp.test(str, XRegExp(item.pattern, "i"))){
+      return true;
+    }
+  });
+
+  if(getResult){
+    return cb({
+      intent: getResult.intent,
+      entities: createEntities(str, getResult.pattern)
+    });
+  } else{
+    return cb({});
+  }
+}; //end of matchpater
+
+module.exports = matchPattern;
