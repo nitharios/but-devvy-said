8934440bd722c506d6c75075c63ac3c56ff9e272
Nathan N <Nitharios@users.noreply.github.com>
2017-11-18 15:33:17 -1000

    Integrated Devvy with Express

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const db = require('./models');
 const DevvyCho = require('./chatbot');
+const dotenv = require('dotenv');
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(DevvyCho);
+// load env variables
+dotenv.load();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+// connects Devvy to slack
+DevvyCho(process.env.SLACK_TOKEN);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
