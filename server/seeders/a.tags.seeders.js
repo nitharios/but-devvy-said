1a5341da0c523697855bbe0ec552610ad53333ce
Nathan N <Nitharios@users.noreply.github.com>
2017-11-19 13:13:50 -1000

    Updated models and seeders

---
+++ server/seeders/a.tags.seeders.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+  
+    return queryInterface.bulkInsert('tags', [
+      {
+        name : 'fundamental'
+      },
+      {
+        name : 'intermediate'
+
+      },
+      {
+        name : 'advanced'
+      },
+      {
+        name : 'back end'
+      },
+      {
+        name : 'front end'
+      },
+      {
+        name : 'library'
+      },
+      {
+        name : 'framework'
+      },
+      {
+        name : 'database'
+      },
+      {
+        name : 'browser'
+      }
+
+    ], {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkDelete('tags', null, {});
+
+  }
+};
