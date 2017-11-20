8e5e1551b5e3baac4fdaccc38345491ba2af8881
Nathan N <Nitharios@users.noreply.github.com>
2017-11-20 09:43:24 -1000

    Cleaning up code

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,6 @@ const Resources = require('./Resources');
 const Tags = require('./Tags');
 const Topics = require('./Topics');
 
-router.get('/', (req, res) => {
-  console.log('Route request');
-  res.json('Hello World!');
-});
-
 router.use('/resources', Resources);
 router.use('/tags', Tags);
 router.use('/topics', Topics);
