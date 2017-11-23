dcdbfbcf48a1f859475ce4aa009d87450aae4ec8
Nathan N <Nitharios@users.noreply.github.com>
2017-11-22 22:28:15 -1000

    Impriving responsiveness

--- chatbot/lib/responses/user.responses.js
+++ chatbot/lib/responses/user.responses.js
@@ -1,5 +1,5 @@
 module.exports = {
-  yes : /^(yes|yea|yup|yep|ya|sure|ok|y|yeah|yah|affirmative)/i,
-  no : /^(no|nah|nope|n|naw|negative|negatory)/i,
+  yes : /^(yes|yea|yup|yep|ya|sure|ok|y|yeah|yah|affirmative|si|hai)/i,
+  no : /^(no|nah|nope|n|naw|negative|negatory|iie)/i,
   quit : /^(quit|cancel|end|stop|done|exit|nevermind|never mind)/i
 };
