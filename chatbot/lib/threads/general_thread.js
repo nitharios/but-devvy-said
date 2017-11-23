2f14dd1e5bb7862e177f16c37e5c8e1af841cf83
Justin Lee <leejwg.3@gmail.com>
2017-11-22 15:42:12 -1000

    before seed

--- chatbot/lib/threads/general_thread.js
+++ chatbot/lib/threads/general_thread.js
@@ -1,21 +1,12 @@
-controller.hears(['question me', 'how are you', 'lets chat', 'hey devvy lets chat'], 'message_received', function(bot,message) {
-
+module.exports = controller.hears(['question me', 'lets chat', 'hey devvy lets chat'], 'message_received', function(bot, message) {
   // start a conversation to handle this response.
-  bot.startConversation(message, function(err,convo) {
-
-    convo.addQuestion('How are you?',function(response, convo) {
-
-      convo.say('Cool, you said: ' + response.text);
-      convo.next();
-
-      controller.hears(['question me'], 'message_received', function(bot,message) {
-
-
+  bot.startConversation(message, function(err, convo) {
 
     convo.addQuestion('Shall we proceed Say YES, NO or DONE to quit.',[
       {
         pattern: 'done',
-        callback: function(response,convo) {
+        callback: function(response, convo) {
+          console.log(convo, 'console log line 9 in threads')
           convo.say('OK you are done!');
           convo.next();
         }
@@ -23,11 +14,14 @@ controller.hears(['question me', 'how are you', 'lets chat', 'hey devvy lets cha
 
       {
         pattern: bot.utterances.yes,
-        callback: function(response,convo) {
+        callback: function(response, convo) {
           convo.say('Great! I will continue...');
-          // do something else...
-          convo.next();
-
+          convo.say('so lets get warmed up and talk about javascript are you ready?');
+          if ('yes'){
+               convo.next();
+          } else{
+            convo.say('ok i hope you study hard :)');
+          }
         }
       },
 
@@ -39,7 +33,6 @@ controller.hears(['question me', 'how are you', 'lets chat', 'hey devvy lets cha
           convo.next();
         }
       },
-
       {
         default: true,
         callback: function(response,convo) {
@@ -51,8 +44,5 @@ controller.hears(['question me', 'how are you', 'lets chat', 'hey devvy lets cha
     ],{},'default');
 
   });
+
 });
-
-
-
-
