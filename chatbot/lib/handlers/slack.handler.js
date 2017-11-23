2845acb57f908c9f727aa5ff4f7572e573752f61
Chris Min <chris.min216@gmail.com>
2017-11-22 18:18:12 -1000

    Change

--- chatbot/lib/handlers/slack.handler.js
+++ chatbot/lib/handlers/slack.handler.js
@@ -27,8 +27,8 @@ module.exports = (function() {
 
     } else if (message.results) {
       console.log(message.results);
-      const { name, Notes } = message.results;
-      bot.reply(message, stringBuilder(name, Notes), (err, response) => {
+      const { name, Resources } = message.results;
+      bot.reply(message, stringBuilder(name, Resources), (err, response) => {
         // response carries the details of the message passed back to the user
         bot.createConversation(message, conversationHandler);
       });
