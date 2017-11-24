e517e7ca70355a159bfe79c696103cd7a98671e0
Nathan N <Nitharios@users.noreply.github.com>
2017-11-24 11:30:30 -1000

    Adding styles

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
 
 export default ({ falseParameter }) => {
   return(
     <div id="nav-bar">
-      This is the nav-bar!
+      <div id="devvy-cho-logo">
+        <Link to="/">
+          <div></div>
+        </Link>
+      </div>
+      <div id="text-nav-links">
+        <Link to="/about">
+          <div>About</div>
+        </Link>
+        <Link to="/addinfo">
+          <div>Add Info</div>
+        </Link>
+        <Link to="/">
+          <div>Extra</div>
+        </Link>
+      </div>
     </div>
   );
 };
