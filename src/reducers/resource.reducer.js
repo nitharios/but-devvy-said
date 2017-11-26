824cb505e9b7d0b99504ea2642148dde7826fc86
Nathan N <Nitharios@users.noreply.github.com>
2017-11-25 14:47:13 -1000

    Cleaning up code

--- src/reducers/resource.reducer.js
+++ src/reducers/resource.reducer.js
@@ -5,7 +5,6 @@ const initialState = [];
 export default (state = initialState, action) => {
   switch(action.type) {
     case QUERY:
-      console.log(action.resourcesList);
       return [ ...action.resourcesList ];
 
     default:
