aa2e3928e05069a48510e3bd235b305126ad034a
Justin Lee <leejwg.3@gmail.com>
2017-11-28 12:17:33 -1000

    added dependencies, created config.js file in config folder

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const debug = require('debug')('botkit:webserver');
 const routes = require('./routes');
-
+const passport = require('passport');
+const session = require('express-session');
 const db = require('./models');
 
 const PORT = process.env.PORT || 8000;
@@ -12,6 +13,13 @@ const app = express();
 app.use(express.static(__dirname + '..' + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* routes */
 app.use('/api', routes);
