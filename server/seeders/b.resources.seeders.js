4fff1d7d3318fac1481c176fee733764e196c4d0
Chris Min <chris.min216@gmail.com>
2017-11-28 08:54:10 -1000

    new resource seed, 3 done with strings for note/example.

--- server/seeders/b.resources.seeders.js
+++ server/seeders/b.resources.seeders.js
@@ -6,40 +6,46 @@ module.exports = {
     return queryInterface.bulkInsert('resources', [
 
       {
-        name : 'Justin',
-        cohort : 19,
-        title : 'What is meant by Asynchronous Javascript?',
-        tags : ['asynchronous', 'promises'],
-        note : 'These are class notes.',
-        example : 'This is a code snippet.',
-        link : 'http://bit.ly/2zOQycP', 
+        name : 'Alexis',
+        cohort : 18,
+        title : 'What is abstraction in computer science?',
+        tags : ['abstraction', 'advanced', 'confusing', 'computer science'],
+        note : 'Abstraction is a technique for arranging the complexity of computer systems. It also has to deal with suppressing the most complex details so that a person can interact with a system more straightforwardly.',
+        example : '', //null
+        link : 'http://bit.ly/2AdTz6r', //YouTube 'Computer Science Concept - Abstraction'
         createdAt : new Date(),
         updatedAt : new Date(),
         topic_id : 1
       },
       {
-        name : 'Chris',
-        cohort : 19,
-        title : 'How to Use Arrow Functions',
-        tags : ['arrow function', 'context'],
-        note : 'These are class notes.',
-        example : 'This is a code snippet.',
-        link : 'http://bit.ly/2zOQycP', 
+        name : 'John',
+        cohort : 16,
+        title : 'What do we use algorithms for?',
+        tags : ['algorithm', 'computer science', 'efficiency', 'sorting', 'big O notation'],
+
+        note : 'An algorithm is a well-defined procedure, or series of steps that allows a computer to solve a problem. It is a lot like a recipe, involving ingredients, or inputs, a series of steps to follow, and a final result, or output.',
+
+        example : '// Set up a simple array of colours var colours = new Array ("Black", "Blue", "Brown", "Green", "Pink", "Red", "White", "Yellow"); // Set up numbers from 1 to 2500 var numbersHalf = new Array(); for (var i = 1; i < 2500; i++) { numbersHalf.push(i); }; // Set up numbers from 1 to 5000 var numbersFull = new Array(); for (var i = 1; i < 5000; i++) { numbersFull.push(i); };', 
+
+        link : 'http://bit.ly/2zOrmnm', //Khan Academy 'What is an algorithm?'
         createdAt : new Date(),
         updatedAt : new Date(),
-        topic_id : 1
+        topic_id : 2
       },
       {
-        name : 'Nathan',
-        cohort : 19,
-        title : 'All About Prototypes and Inheritance',
-        tags : ['prototype', 'inheritance'],
-        note : 'These are class notes.',
-        example : 'This is a code snippet.',
-        link : 'http://bit.ly/2zOQycP', 
+        name : 'Jayce',
+        cohort : 13,
+        title : 'What is Angular?',
+        tags : ['front-end', 'framework', 'javascript'],
+
+        note : 'AngularJS is an extremely powerful framework that front-end web developers rely on to create the interactive aspects of a website. AngularJS uses a very modular approach, and gives developers all they need to set up the front-end of an application. It mainly involves the use of controllers, services, directives, and expressions.',
+
+        example : '', //null
+
+        link : 'http://bit.ly/2AgD4qF', //YouTube JsWiz 'What is AngularJS?'
         createdAt : new Date(),
         updatedAt : new Date(),
-        topic_id : 1
+        topic_id : 3
       }
 
     ], {});
