f756f8081b1dbc2c3c734ef3d5afdff41466a505
Nathan N <Nitharios@users.noreply.github.com>
2017-11-27 19:57:17 -1000

    Adding styles

---
+++ src/components/footer.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div id="footer">
+      <div id="footer-box">
+        <a href="https://www.instagram.com/devvy_cho/" target="_blank" rel="noopener noreferrer">
+          <div className="footer-item" id="instagram"></div>
+        </a>
+        <a href="https://devleague.com" target="_blank" rel="noopener noreferrer">
+          <div className="footer-item" id="devleague"></div>
+        </a>
+        <a href="https://twitter.com/DevvyCho" target="_blank" rel="noopener noreferrer">
+          <div className="footer-item" id="twitter"></div>
+        </a>        
+      </div>
+    </div>
+  )
+};
