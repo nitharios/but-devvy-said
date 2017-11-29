c0b47f619d340b4b0301cc7b65fc574db214a14b
Justin Lee <leejwg.3@gmail.com>
2017-11-28 15:16:39 -1000

    more checks

---
+++ server/auth/config/github.js
@@ -0,0 +1,9 @@
+const ids = {
+  github: {
+    clientID: 'get_your_own', //need 
+    clientSecret: 'get_your_own', //need 
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  }
+};
+
+module.exports = ids;
