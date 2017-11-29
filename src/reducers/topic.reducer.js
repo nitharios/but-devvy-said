da718015db9902dc3325d99b61ebf3dcc245a78d
Justin Lee <leejwg.3@gmail.com>
2017-11-29 10:50:56 -1000

    place holder blank in drop down menue

--- src/reducers/topic.reducer.js
+++ src/reducers/topic.reducer.js
@@ -1,12 +1,13 @@
 import { LOAD_TOPICS } from '../actions/topic.actions';
 
 const initialState = [];
+const placeHolder = 'Choose your Topic';
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case LOAD_TOPICS:
       console.log("LOAD");                 
-      return [ ...action.topicsList ];
+      return [ placeHolder, ...action.topicsList ];
 
     default:
       return state;
