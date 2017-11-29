cf43b3135d40551bc508fb411b8bfcfc1154522f
Justin Lee <leejwg.3@gmail.com>
2017-11-29 09:49:03 -1000

    checking for bugs front endd

--- src/components/validate.component.js
+++ src/components/validate.component.js
@@ -1,10 +1,10 @@
 const validate = values => {
   const errors = {};
-
+  
   if(!values.title) {
     errors.title = 'required';
   
-  } else if (values.title.length <= 5) {
+  } else if (values.title.length <= 5 || values.title.length === 0) {
     errors.title = 'too short';
   }
 
