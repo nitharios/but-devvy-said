931ccb58386931ccd566fe731516cd30d36b1d3b
Justin Lee <leejwg.3@gmail.com>
2017-11-29 16:37:51 -1000

    404 basic box kitty

--- src/components/404.component.js
+++ src/components/404.component.js
@@ -5,8 +5,8 @@ import './404.css';
 const NotFound = () =>
   <div className='container-404'>
     <h3>404 page not found</h3>
-    <p className='404-announce'>Devvy Here! Your page does not seem to exist :(  .</p>
-    <p className='404-denounce'>we do however have kittens.</p>
+    <p>Devvy Here! Your page does not seem to exist :(</p>
+    <p>Try Heading back back!</p>
     <iframe src="https://giphy.com/embed/xT0GqtpF1NWd9VbstO" width="361" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
   </div>
 
