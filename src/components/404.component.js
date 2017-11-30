ae5e65644aed2aa5fa91148916024180b683b45c
Justin Lee <leejwg.3@gmail.com>
2017-11-29 16:09:21 -1000

    adding stlying to 404

--- src/components/404.component.js
+++ src/components/404.component.js
@@ -1,10 +1,13 @@
 // components/NotFound.js
 import React from 'react';
+import './404.css';
 
 const NotFound = () =>
-  <div>
+  <div className='container-404'>
     <h3>404 page not found</h3>
-    <p>We are sorry but the page you are looking for does not exist.</p>
+    <p className='404-announce'>Devvy Here! Your page does not seem to exist :(  .</p>
+    <p className='404-denounce'>we do however have kittens.</p>
+    <iframe src="https://giphy.com/embed/xT0GqtpF1NWd9VbstO" width="361" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
   </div>
 
 export default NotFound;
