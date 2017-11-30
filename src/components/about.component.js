b07308e8650f818cbf93cde746b9773ef68057a6
Justin Lee <leejwg.3@gmail.com>
2017-11-30 12:28:11 -1000

    more formatting;

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -9,7 +9,13 @@ export default ({ falseParameter }) => {
 
       <div className="content">
         <div>
-          Aloha! My name is Devvy Cho! I am the browser-implementation of a bot designed by <a href="https://github.com/chrismin217" target="_blank" rel="noopener noreferrer">Chris Min</a>, <a href="https://github.com/jlee709" target="_blank" rel="noopener noreferrer"> Justin Lee </a>  and <a href="https://github.com/Nitharios" target="_blank" rel="noopener noreferrer">Nathaniel Niosco</a> as a part of their final project for dev leagues Cohort 19. I am also a Slack Bot, created for the purposeful use of the DevLeague Community. 
+          Aloha! 
+        </div>
+        <div>
+          My name is Devvy Cho! and im glad you came to my about page to learn more about this project and our team!
+        </div>
+        <div>
+          I am the browser-implementation of a bot designed by <a href="https://github.com/chrismin217" target="_blank" rel="noopener noreferrer">Chris Min</a>, <a href="https://github.com/jlee709" target="_blank" rel="noopener noreferrer"> Justin Lee </a>  and <a href="https://github.com/Nitharios" target="_blank" rel="noopener noreferrer">Nathaniel Niosco</a> as a part of their final project for dev leagues Cohort 19. I am also a Slack Bot, created for the purposeful use of the DevLeague Community. 
         </div>
         <div>
           My core technologies utilize <a href="https://wit.ai" target="_blank" rel="noopener noreferrer">Wit.ai</a>, a natural language processor (NLP) that assists in turning conversational messages into actionable commands. Using Wit enabled the team build different versions of me with ease. This version of me was built using the <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React.js</a> library, with a bit of help from additional libraries and packages. Check out my <a href="https://github.com/nitharios/but-devvy-said" target="_blank" rel="noopener noreferrer">GitHub</a> if you want to learn more!
