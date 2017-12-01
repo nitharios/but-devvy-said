d866d6c6b9309c102076d1d2a02d4b4f61e87b2c
Justin Lee <leejwg.3@gmail.com>
2017-12-01 11:39:16 -1000

    changes to seeder

--- server/seeders/o.resources_14.seeders.js
+++ server/seeders/o.resources_14.seeders.js
@@ -86,8 +86,77 @@ app.use('/user/:id', function (req, res, next) {
         createdAt: new Date(),
         updatedAt: new Date(),
         topic_id:  150 // router middlewawre
-      }
+      },
+      {
+        name: 'bugs runner',
+        cohort: 19,
+        title:'what is a runtime environment',
+        tags:['runtime environment'],
+        note:' As soon as a software program is executed, it is in a runtime state. In this state, the program can send instructions to the computers processor and access the computers memory (RAM) and other system resources. \n Run time environment - Everything you need to execute a program, but no tools to change it.',
+        example:`
+see link ^^ -devvy`,
+        link: 'https://mzl.la/2zUZLB9',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id:  151 // runtime environment
+      },
+      {
+        name: 'listorien',
+        cohort: 19,
+        title:'what is scope',
+        tags:['scope'],
+        note:'Scope determines the accessibility (visibility) of variables. \n In JavaScript there are two types of scope: \n local scope \n global scope \n JavaScript has function scope: Each function creates a new scope. Scope determines the accessibility (visibility) of these variables. Variables defined inside a function are not accessible (visible) from outside the function.',
+        example:`
+// Global Scope
+function someFunction() {
+    // Local Scope #1
+    function someOtherFunction() {
+        // Local Scope #2
+    }
+}
 
+// Global Scope
+function anotherFunction() {
+    // Local Scope #3
+}
+// Global Scope`,
+        link:'https://scotch.io/tutorials/understanding-scope-in-javascript',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id:  152 // scope
+      },
+      {
+        name: 'semiwan',
+        cohort: 19,
+        title:'when to use semi-colon',
+        tags:['semi-colon'],
+        note:'The semicolon in JavaScript is used to separate statements, but it can be omitted if the statement is followed by a line break (or theres only one statement in a  block . A statement is a piece of code that tells the computer to do something.',
+        example:`
+let matchPattern = (str, cb) => {
+  let getResult = patterns.find(item => {
+    if(XRegExp.test(str, XRegExp(item.pattern, "i"))){
+      return true;
+    }
+  });`,
+
+        link: 'http://bit.ly/2kfKVy0',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id: 153 // semi-colon
+      },
+      {
+        name:'seeker joe',
+        cohort: 19,
+        title:'what is sequelize js',
+        tags:['sequelize'],
+        note:'Sequelize.js is an ORM (Object/Relational Mapper) which provides easy access to MySQL, MariaDB, SQLite or PostgreSQL databases by mapping database entries to objects and vice versa. It has very powerful migrations mechanism that can transform existing database schema into a new version. It also provides database synchronization mechanisms that can create database structure by specifying the model structure.',
+        example:`
+see link ^^ -devvy`,
+        link:'http://bit.ly/2kgoglh',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id: 154 // sequelize js
+      }
 
     ], {});
 
