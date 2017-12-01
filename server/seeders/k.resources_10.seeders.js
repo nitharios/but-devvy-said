7dae1d80e270edd890db46d845a9ba3db6293745
Justin Lee <leejwg.3@gmail.com>
2017-12-01 09:59:57 -1000

    fixed bug added seeders

--- server/seeders/k.resources_10.seeders.js
+++ server/seeders/k.resources_10.seeders.js
@@ -7,58 +7,58 @@ module.exports = {
 
   up : (queryInterface, Sequelize) => {
 
-    return queryInterface.bulkInsert('resources', [
+  //   // return queryInterface.bulkInsert('resources', [
 
-      {
-        name : 'Debbie',
-        cohort : 7,
-        title : 'What is a merge conflict?',
-        tags : ['github', 'merge conflict', 'git terminal'],
-        note : 'A merge conflict happens when integrating another branch into your current working branch. Often times, one person will delete a file, while the other person has modified it. Git cannot know what is correct, and this is when a merge conflict occurs.',
+  //     {
+  //       name : 'Debbie',
+  //       cohort : 7,
+  //       title : 'What is a merge conflict?',
+  //       tags : ['github', 'merge conflict', 'git terminal'],
+  //       note : 'A merge conflict happens when integrating another branch into your current working branch. Often times, one person will delete a file, while the other person has modified it. Git cannot know what is correct, and this is when a merge conflict occurs.',
 
-        example : null,
+  //       example : null,
 
-        link : 'http://bit.ly/2ieOnbS', //GitTower
-        createdAt : new Date(),
-        updatedAt : new Date(),
-        topic_id : 106 //'merge conflict'
-      },
-      {
+  //       link : 'http://bit.ly/2ieOnbS', //GitTower
+  //       createdAt : new Date(),
+  //       updatedAt : new Date(),
+  //       topic_id : 106 //'merge conflict'
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
 
-      },
-      {
+  //     },
+  //     {
         
-      }
+  //     }
 
-    ], {});
+  //   ], {});
 
-  },
+  // },
 
-  down : (queryInterface, Sequelize) => {
+  // down : (queryInterface, Sequelize) => {
 
-    return queryInterface.bulkDelete('resources', null, {});
+  //   // return queryInterface.bulkDelete('resources', null, {});
 
-  }
+   }
 
 };
