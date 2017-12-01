14234f7eda59ac65d01df8ef2efc9128430695d1
Justin Lee <leejwg.3@gmail.com>
2017-12-01 11:16:21 -1000

    more data

--- server/seeders/o.resources_14.seeders.js
+++ server/seeders/o.resources_14.seeders.js
@@ -22,7 +22,21 @@ module.exports = {
         updatedAt: new Date(),
         topic_id:  146 // relational database management system
       },
+      {
+        name: 'gruntal grinds',
+        cohort: 4,
+        title: 'what is a relational operator',
+        tags:['relational operator'],
+        note:'In computer science, a relational operator is a programming language construct or operator that tests or defines some kind of relation between two entities. These include numerical equality (e.g., 5 = 5) and inequalities (e.g., 4 ≥ 3).\n In programming languages that include a distinct boolean data type in their type system, like Pascal, Ada, or Java, these operators usually evaluate to true or false, depending on if the conditional relationship between the two operands holds or not. In languages such as C, relational operators return the integers 0 or 1, where 0 stands for false and any non-zero value stands for true. \n An expression created using a relational operator forms what is termed a relational expression or a condition. Relational operators can be seen as special cases of logical predicates.',
+        example:`
+see link ^^ -devvy`,
+        link: 'http://bit.ly/2i9GKjh',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id:  147// relational operator
+      },
       
+
 
     ], {});
 
