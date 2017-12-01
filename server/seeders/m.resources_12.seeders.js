9b3dbc69abac12218079172098da453f7c191f7e
Justin Lee <leejwg.3@gmail.com>
2017-12-01 09:12:55 -1000

    adding data

--- server/seeders/m.resources_12.seeders.js
+++ server/seeders/m.resources_12.seeders.js
@@ -9,6 +9,36 @@ module.exports = {
 
     return queryInterface.bulkInsert('resources', [
 
+      {
+        name: 'victor bot',
+        cohort: 4,
+        title: 'using parenthesis',
+        tags:['parens', 'parenthesis'],
+        note:'are used to wrap params in JS and can also wrap functions such as an IIFE (Immediatly Invoked Function Expression',
+        example:`
+(function() {
+    var Dom = YAHOO.util.Dom,
+    Event = YAHOO.util.Event,
+    layout = null,
+})();`,
+
+        link:'https://mzl.la/1NY4S1G',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id: 126 // parenthesis
+      },
+      {
+        name: 'jackobyte',
+        cohort: 1,
+        title:'pass by reference',
+        tags:[' pass by reference'],
+        note: 'Primitives are passed by value, Objects are passed by "copy of a reference".Specifically, when you pass an object (or array) you are (invisibly) passing a reference to that object, and it is possible to modify the contents of that object, but if you attempt to overwrite the reference it will not affect the copy of the reference held by the caller - i.e. the reference itself is passed by value:',
+        example:``
+        link:
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        topic_id: 127 //
+      }
 
     ], {});
 
