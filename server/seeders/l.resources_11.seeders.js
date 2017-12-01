7a0e3ce8293ed48cc24fe5aa81075dbfc0aa48f4
Chris Min <chris.min216@gmail.com>
2017-11-30 21:09:04 -1000

    init

--- server/seeders/l.resources_11.seeders.js
+++ server/seeders/l.resources_11.seeders.js
@@ -9,6 +9,20 @@ module.exports = {
 
     return queryInterface.bulkInsert('resources', [
 
+      {
+        name : 'Ricky',
+        cohort : 19,
+        title : 'What do they mean by Object Oriented Programming?',
+        tags : ['object oriented programming', 'paradigm', 'objects'],
+        note : 'Objected Oriented Programming, or OOP, is a programming paradigm. Before OOP, most programs were simply a procedural list of instructions. The essence of OOP is the intricate interaction of many objects, each with state and behavior. Classes or prototypes generate, or initialize these objects, as well as define their structure much like a blueprint.',
+
+        example : null,
+
+        link : 'http://bit.ly/2AE3sLq', //StackExchange -- 'OOP for the non-technical'
+        createdAt : new Date(),
+        updatedAt : new Date(),
+        topic_id : 121 //'object oriented programming'
+      }
 
     ], {});
 
--- server/seeders/m.resources_12.seeders.js
+++ server/seeders/m.resources_12.seeders.js
@@ -9,6 +9,19 @@ module.exports = {
 
     return queryInterface.bulkInsert('resources', [
 
+      {
+        name : 'Edward',
+        cohort : 9,
+        title : 'What does it mean to pass by reference?',
+        tags : ['objects', 'pass by reference', 'variables'],
+        note : 'Variables that are assigned a non-primitive value are given a reference to that value. It is like passing a remote control pointing to that non-primitive into the variable. The remote control points to the memory location.',
+
+        example : null,
+        link : 'http://bit.ly/2AcyVmS',
+        createdAt : new Date(),
+        updatedAt : new Date(),
+        topic_id : 127 //'pass by reference'
+      }
 
     ], {});
 
